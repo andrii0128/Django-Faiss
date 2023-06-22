be7bf27159ad0da85fdb7d098c64a9c53212793d
andrii0128 <kostenkoandrii.dev@gmail.com>
2023-06-22 10:47:30 -0700

    Similarity Search update

--- doot/views.py
+++ doot/views.py
@@ -52,38 +52,12 @@ def get_embedding(request):
 def similar_search(request):
     if request.method == 'GET':
         utterance = request.query_params.get("utterance")
-        # docs = []
-        # dataset = Utterance.objects.all()
-        # for data in dataset:
-        #     item = Document(page_content=data.utterance, metadata=dict(intent=data.intent_category))
-        #     docs.append(item)
-        docs = [
-            Document(page_content='Can you provide me an estimate for painting my living room?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="I'm interested in a quote for repainting the exterior of my house.", metadata={'intent': 'Request_Quote'}),
-            Document(page_content='How much would it cost to paint my kitchen cabinets?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='I would like a proposal for a fresh coat of paint in my bedrooms.', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='Can you give me a price for painting my basement?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="What's the cost to paint the ceiling in my hallway?", metadata={'intent': 'Request_Quote'}),
-            Document(page_content='Can you provide a quote for painting my basement, including repairing some water damage?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="I need an estimate for repainting my home's exterior and replacing a few damaged siding panels.", metadata={'intent': 'Request_Quote'}),
-            Document(page_content="What's the cost for painting the interior of a 4-bedroom house?", metadata={'intent': 'Request_Quote'}),
-            Document(page_content="Could you give me a quote to paint my home's exterior, including the garage?", metadata={'intent': 'Request_Quote'}),
-            Document(page_content='I need a quote for painting my hallway and fixing some cracked plaster.', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='How much would you charge to paint my kitchen and repair some water stains on the ceiling?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="What's the cost of painting the interior of a 2000 square foot house?", metadata={'intent': 'Request_Quote'}),
-            Document(page_content='Can I get a quote for painting the exterior of my single-story house and repairing some rotten wood?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="I'd like a quote for painting my master bedroom, including the closet.", metadata={'intent': 'Request_Quote'}),
-            Document(page_content='Can you provide a cost estimate to paint my living room and dining room, along with some minor drywall repairs?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='How much would it be to paint the exterior of my house and repair some chipped paint?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='Could you estimate the cost to repaint all my exterior doors and window frames?', metadata={'intent': 'Request_Quote'}),
-            Document(page_content='I need a quote for painting all the interior doors in my home.', metadata={'intent': 'Request_Quote'}),
-            Document(page_content="What's the cost to paint my house's exterior, including repairing a few areas of damaged stucco?", metadata={'intent': 'Request_Quote'}),
-            Document(page_content="Can you give me an estimate for painting my home's entire interior and repairing some wall cracks?", metadata={'intent': 'Request_Quote'}),
-        ]
+        docs = []
+        dataset = Utterance.objects.all()
+        for data in dataset:
+            item = Document(page_content=data.utterance, metadata=dict(intent=data.intent_category))
+            docs.append(item)
 
         result = similar_search_func(docs, utterance, openai_api_key)        
-        # for item in docs:
-        #     print(f"Document({item}),")
-        # result = docs[0]
         response = f"Utterance:{result.page_content}, Metadata:{result.metadata}"
         return Response(response)
